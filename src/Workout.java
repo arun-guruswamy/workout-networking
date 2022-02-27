932625c66241f0a3cf4ebdfe70fcf3b6ddb881ff
Ryan <rmetz@vassar.edu>
2022-02-27 14:06:44 -0500

    Added the general attributes to the toString of the Cardio and strength class

--- src/Workout.java
+++ src/Workout.java
@@ -14,7 +14,7 @@ public abstract class Workout {
     public void setGeneralAttributes() {
         Scanner scan = new Scanner(System.in);
         System.out.println("How long is your workout? (Enter as a number in the format: 000 " +
-                "with the first digit being hours and the last two digits minutes");
+                "with the first digit being hours and the last two digits minutes)");
         while(true) {
             try {
                 this.length = Integer.parseInt(scan.next());
@@ -25,7 +25,7 @@ public abstract class Workout {
             }
         }
 
-        System.out.println("How do you rate the difficulty of this workout? (A number from 1 to 5");
+        System.out.println("How do you rate the difficulty of this workout? (A number from 1 to 5)");
         while(true) {
             try {
                 this.difficulty= Integer.parseInt(scan.next());
