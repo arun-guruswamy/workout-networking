38eae2111128429cd33887afe9786a772beb7054
Ryan <rmetz@vassar.edu>
2022-03-29 16:57:00 -0400

    attempt at create post view and some logic behind create post

--- app/src/main/java/edu/vassar/cmpu203/workoutapp/Controller/MainActivity.java
+++ app/src/main/java/edu/vassar/cmpu203/workoutapp/Controller/MainActivity.java
@@ -8,10 +8,11 @@ import android.os.Bundle;
 
 import edu.vassar.cmpu203.workoutapp.R;
 import edu.vassar.cmpu203.workoutapp.View.Create_Post_Fragment;
+import edu.vassar.cmpu203.workoutapp.View.ICreatePostView;
 import edu.vassar.cmpu203.workoutapp.View.IMainView;
 import edu.vassar.cmpu203.workoutapp.View.MainView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ICreatePostView.Listener {
 
     private Post post;
     private IMainView mainView;
@@ -24,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(this.mainView.getRootView());
         this.mainView.displayFragment(new Create_Post_Fragment(), true);
+    }
+
+    @Override
+    public void onAddedCaption(String caption) {
+        post.addCaption(caption);
     }
 }
---
+++ app/src/main/java/edu/vassar/cmpu203/workoutapp/View/ICreatePostView.java
@@ -0,0 +1,13 @@
+package edu.vassar.cmpu203.workoutapp.View;
+
+import android.view.View;
+
+import edu.vassar.cmpu203.workoutapp.Model.*;
+
+public interface ICreatePostView {
+
+
+    interface Listener{void onAddedCaption(String caption); }
+    View getRootView();
+
+}
