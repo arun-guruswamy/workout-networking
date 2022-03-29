0d3a4f428261e5246805170b084a6928d3ab2982
Ryan <rmetz@vassar.edu>
2022-03-29 15:15:45 -0400

    some profile class work

--- app/src/main/java/edu/vassar/cmpu203/workoutapp/Model/Profile.java
+++ app/src/main/java/edu/vassar/cmpu203/workoutapp/Model/Profile.java
@@ -5,6 +5,36 @@ public class Profile {
     private String username;
     private String password;
     private Feed posts;
+    private String name;
+    private String bio;
+    private int experienceLevel;
 
+    Profile() {
+        this.posts = new Feed();
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setExperienceLevel(int experienceLevel) {
+        if (experienceLevel <= 5 || experienceLevel >= 0)
+          this.experienceLevel = experienceLevel;
+    }
 }
