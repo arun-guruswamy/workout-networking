6d214c45b53a2b6d5836353a91490fcaf892174d
Ryan <rmetz@vassar.edu>
2022-04-13 11:31:16 -0400

    create profile snackbar test

--- app/src/androidTest/java/edu/vassar/cmpu203/workoutapp/CreateProfileInstTest.java
+++ app/src/androidTest/java/edu/vassar/cmpu203/workoutapp/CreateProfileInstTest.java
@@ -1,5 +1,6 @@
 package edu.vassar.cmpu203.workoutapp;
 
+import android.os.SystemClock;
 import android.view.View;
 
 import androidx.test.espresso.Espresso;
@@ -10,6 +11,8 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -52,5 +55,74 @@ public class CreateProfileInstTest {
 //        profile.setPassword("abcdefg123!!");
 //        profile.setBio("I love to workout");
 //        return profile;
+    }
+
+    /**
+     * Test to make sure that the snackbar will pop up
+     * when all the needed data is not entered
+     */
+    @Test
+    public void testSnackbar() {
+        //Enter a blank username but has a password and a bio
+        ViewInteraction usernameVi = Espresso.onView(ViewMatchers.withId(R.id.UsernameText));
+        usernameVi.perform(ViewActions.replaceText(""));
+        ViewInteraction createButtonVi = Espresso.onView(ViewMatchers.withId(R.id.createButton));
+        createButtonVi.perform(ViewActions.click());
+
+        //the snackbar matcher
+        Matcher<View> snackbarMatcher = ViewMatchers.withText("Username, password, and bio are mandatory!");
+       ViewInteraction snackBarVi = Espresso.onView(snackbarMatcher);
+       snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+       //sleep between each test so that the snackbar will go away
+       SystemClock.sleep(3500);
+
+       //Have entered a bio and username but no password
+        ViewInteraction bioVi = Espresso.onView(ViewMatchers.withId(R.id.bioEditText));
+        bioVi.perform(ViewActions.replaceText("Bio"));
+        ViewInteraction passwordVi = Espresso.onView(ViewMatchers.withId(R.id.passwordEditText));
+        passwordVi.perform((ViewActions.replaceText("")));
+        usernameVi.perform(ViewActions.replaceText("Username"));
+        createButtonVi.perform(ViewActions.click());
+
+        snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+        SystemClock.sleep(3500);
+
+        //entered user name and password but no bio
+        bioVi.perform(ViewActions.replaceText(""));
+        passwordVi.perform(ViewActions.replaceText("password"));
+        usernameVi.perform(ViewActions.replaceText("Username"));
+        createButtonVi.perform(ViewActions.click());
+        snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+        SystemClock.sleep(3500);
+
+        //did not enter any of the required data
+        createButtonVi.perform(ViewActions.click());
+        snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+        SystemClock.sleep(3500);
+
+        //entered just a password
+        passwordVi.perform(ViewActions.replaceText("password"));
+        createButtonVi.perform(ViewActions.click());
+        snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+        SystemClock.sleep(3500);
+
+        //only entered a username
+        usernameVi.perform(ViewActions.replaceText("Username"));
+        createButtonVi.perform(ViewActions.click());
+        snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+        SystemClock.sleep(3500);
+
+        //only entered a username
+        bioVi.perform(ViewActions.replaceText("Bio"));
+        createButtonVi.perform(ViewActions.click());
+        snackBarVi.check(ViewAssertions.matches(snackbarMatcher));
+
+
     }
 }
