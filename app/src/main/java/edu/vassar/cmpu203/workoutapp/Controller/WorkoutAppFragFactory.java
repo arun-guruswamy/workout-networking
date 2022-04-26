8b42b9863a3bc6b4c80224dfc6bd60c1dcc0889a
Arun <aguruswamy@vassar.edu>
2022-04-26 16:00:39 -0400

    updated fragFactory

--- app/src/main/java/edu/vassar/cmpu203/workoutapp/Controller/WorkoutAppFragFactory.java
+++ app/src/main/java/edu/vassar/cmpu203/workoutapp/Controller/WorkoutAppFragFactory.java
@@ -5,6 +5,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentFactory;
 
 import edu.vassar.cmpu203.workoutapp.View.AddWorkoutFragment;
+import edu.vassar.cmpu203.workoutapp.View.CardioFragment;
+import edu.vassar.cmpu203.workoutapp.View.CreateProfileFragment;
+import edu.vassar.cmpu203.workoutapp.View.Create_Post_Fragment;
+import edu.vassar.cmpu203.workoutapp.View.FeedFragment;
+import edu.vassar.cmpu203.workoutapp.View.FilterFragment;
+import edu.vassar.cmpu203.workoutapp.View.HomeScreenFragment;
+import edu.vassar.cmpu203.workoutapp.View.MobilityFragment;
+import edu.vassar.cmpu203.workoutapp.View.StrengthFragment;
+import edu.vassar.cmpu203.workoutapp.View.ViewProfileFragment;
 
 public class WorkoutAppFragFactory extends FragmentFactory {
 
@@ -14,14 +23,32 @@ public class WorkoutAppFragFactory extends FragmentFactory {
         this.controller = controller;
     }
 
-   /* @NonNull
-    @Override
-    public Fragment instantiate(@NonNull ClassLoader classLoader, @NonNull String className){
-
-        Class<? extends Fragment> fragClass = loadFragmentClass(classLoader, className);
-
-        if(fragClass == AddWorkoutFragment.class)
-            return new AddWorkoutFragment(controller);
-    }*/
+//    @NonNull
+//    @Override
+//    public Fragment instantiate(@NonNull ClassLoader classLoader, @NonNull String className){
+//
+//        Class<? extends Fragment> fragClass = loadFragmentClass(classLoader, className);
+//
+//        if(fragClass == AddWorkoutFragment.class)
+//            return new AddWorkoutFragment(controller);
+//        if(fragClass == FeedFragment.class)
+//            return new FeedFragment(controller);
+//        if(fragClass == Create_Post_Fragment.class)
+//            return new Create_Post_Fragment(controller);
+//''      if(fragClass == FilterFragment.class)
+//            return new FilterFragment(controller);
+//        if(fragClass == HomeScreenFragment.class)
+//            return new HomeScreenFragment(controller);
+//        if(fragClass == CreateProfileFragment.class)
+//            return new CreateProfileFragment(controller);
+//        if(fragClass == CardioFragment.class)
+//            return new CardioFragment(controller);
+//        if(fragClass == StrengthFragment.class)
+//            return new StrengthFragment(controller);
+//        if(fragClass == MobilityFragment.class)
+//            return new MobilityFragment(controller);
+//        if(fragClass == ViewProfileFragment.class)
+//            return new ViewProfileFragment(controller);
+//    }
 
 }
